e00736941796a234aec4e849bef241cd80a72f40
App Academy Student <student@appacademy.io>
2013-12-02 13:25:52 -0800

    perform_jump and perform_slide fully functional

--- board.rb
+++ board.rb
@@ -1,30 +1,91 @@
+# encoding: utf-8
 require_relative 'piece'
 require 'colorize'
 
 class Board
-
   def initialize
     @grid = Array.new(8) { Array.new(8) }
   end
 
-  def render
-    puts "  0 1 2 3 4 5 6 7"
+  def to_s
+    board_render = "  0 1 2 3 4 5 6 7\n"
     8.times do |row_index|
-      print "#{row_index} "
+      board_render += "#{row_index} "
       8.times do |col_index|
         background = ((row_index + col_index) % 2 == 0) ? :light_blue : :light_green
         piece = self[row_index, col_index]
         to_print = piece.nil? ? "  " : piece.to_s
-        print to_print.colorize(background: background)
+        board_render += to_print.colorize(background: background)
       end
-      puts
-    end  end
+      board_render += "\n"
+    end  
+    board_render + "\n"
+  end
 
   def [](row, col)
     @grid[row][col]
   end
 
+  def dup
+    dup_board = Board.new
+
+    all_pieces.each do |piece|
+      new_piece = Piece.new( piece.position, piece.color, dup_board, piece.king )
+      dup_board.add_piece(new_piece, new_piece.position)
+    end
+
+    dup_board
+  end
+
+  def all_pieces(color = nil)
+    return @grid.flatten.compact if color == nil
+    @grid.flatten.compact.select { |piece| piece.color == color }
+  end
+
   def []=(row, col, piece)
     @grid[row][col] = piece
   end
+
+  def empty_pos?(pos)
+    self[*pos] == nil
+  end
+
+  def move(piece, new_pos)
+    remove_piece(piece)
+    add_piece(piece, new_pos)
+  end
+
+  def add_piece(piece, new_pos)
+    self[ new_pos[0], new_pos[1] ] = piece
+    piece.move( new_pos )
+  end
+
+  def remove_piece(piece)
+    old_pos = piece.position
+    self[ *old_pos ] = nil
+    piece.position = nil
+  end
 end
+
+b = Board.new
+p1 = Piece.new([4, 4], :b, b, true)
+p2 = Piece.new([5, 5], :w, b, true)
+p3 = Piece.new([3, 3], :w, b, true)
+
+
+b.add_piece(p1, p1.position)
+b.add_piece(p2, p2.position)
+
+
+puts b
+
+# b.render
+
+# # p1.perform_jump([6, 6])
+# # b.render
+
+# p p1.valid_slides
+# p1.perform_slide([5, 3])
+
+p1.perform_moves!([5, 3])
+puts b
--- piece.rb
+++ piece.rb
@@ -1,24 +1,101 @@
 # encoding: utf-8
-require_relative 'board'
+
+require 'matrix'
+
+class InvalidMoveError < StandardError
+end
 
 class Piece
   attr_accessor :position, :color, :board, :king
 
   SLIDE_OFFSETS = [ [1, 1], [1, -1] ]
-  JUMP_OFFSETS  = [ [2, 2], [2, -1] ] 
+  JUMP_OFFSETS  = [ [2, 2], [2, -2] ] 
 
   def initialize(position, color, board, king = false)
     @position, @color, @board, @king = position, color, board, king
   end
+
+  def to_s
+    color == :b ? "⚫ " : "⚪ "
+  end
+
+  def perform_moves!(move_sequence)
+    if move_sequence.length == 1
+      if is_valid_slide?( move )
+        perform_slide ( move )
+      else
+        perform_jump ( move )
+      end
+    else
+      move_sequence.each do |move|
+        perform_jump( move )
+      end
+    end
+  end
+
+  def is_valid_sequence?(move_sequence)
+    new_board = Board.dup
+    # find current piece
+    # perform_moves! on current piece but on the dup board
+    # if this raises an error, then return false, otherwise true
+  end
+
+  def perform_slide(new_pos)
+    raise InvalidMoveError.new("not a valid slide") unless valid_slide?( new_pos )
+    @board.move(self, new_pos)
+  end
+
+  def perform_jump(new_pos)
+    raise InvalidMoveError.new("not a valid jump") unless valid_jump?( new_pos )
+
+    remove_middle_piece( new_pos )
+
+    @board.move( self, new_pos )
+
+    maybe_promote
+  end
+
+
+  def valid_slides
+    potential_slides.select { |new_pos| @board.empty_pos?( new_pos ) }
+  end
+
+  def valid_jumps
+    potential_jumps.select! { |new_pos| @board.empty_pos?( new_pos ) }
+
+    [].tap do |arr|
+      potential_jumps.each do |jump|
+        middle_pos = between_pos( @position, jump )
+        piece = @board[ middle_pos[0], middle_pos[1] ]
+        arr << jump if (!piece.nil? && piece.color != @color)
+      end
+    end
+  end
+
+  def move(new_pos)
+    @position = new_pos
+  end
+
+
+  private
 
   def directions
     return [1, -1] if @king
     @color == :b ? [1] : [-1]
   end
 
-  def slide_offsets
+  # black back row is 0, white back row is 7
+  def opponent_back_row
+    @color == :b ? 7 : 0
+  end
+
+  def maybe_promote
+    @king = true if @position[0] == opponent_back_row
+  end
+
+  def move_offsets_with_direction(move_type_offsets)
     [].tap do |arr|
-      SLIDE_OFFSETS.map do |offset|
+      move_type_offsets.map do |offset|
         directions.map do |direction|
           arr << [ offset[0] * direction, offset[1] ]
         end
@@ -26,7 +103,51 @@ class Piece
     end
   end
 
-  # def jump_offsets
-  #   JUMP_OFFSETS.map { |offset| offset[1] *= direction }
-  # end
-end
+  def between_pos(pos1, pos2)
+    ( Vector[ *pos1 ] + Vector[ *pos2 ] ) / 2
+  end
+
+  def potential_slides
+    potential_moves( slide_offsets )
+  end
+
+  def potential_jumps
+    potential_moves( jump_offsets )
+  end
+
+  def potential_moves(move_type_offsets)
+    [].tap do |arr|
+      move_type_offsets.each do |slide_offset|
+        new_pos = ( Vector[ *@position ] + Vector[ *slide_offset ] ).to_a
+        arr << new_pos if in_bounds?(new_pos)
+      end
+    end
+  end
+
+  def in_bounds?(coords)
+    coords.all? { |coord| coord.between?( 0, 7 ) }
+  end
+
+  def slide_offsets
+    move_offsets_with_direction(SLIDE_OFFSETS)
+  end
+
+  def jump_offsets
+    move_offsets_with_direction(JUMP_OFFSETS)
+  end
+
+  def remove_middle_piece(new_pos)
+    pos_to_remove =   between_pos( @position, new_pos )
+    piece_to_remove = @board[ *pos_to_remove ]
+    @board.remove_piece( piece_to_remove )
+    maybe_promote
+  end
+
+  def valid_slide?(new_pos)
+    valid_slides.include?( new_pos )
+  end
+
+  def valid_jump?(new_pos)
+    valid_jumps.include?( new_pos )
+  end
+end 
